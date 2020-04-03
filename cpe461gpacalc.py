431cbd00fe00a53f8c5b613fd96c78552e311598
Nwaobi Daniel <iamdharnielle@localhost.localdomain>
2020-04-03 10:21:59 +0100

    cpe461gpacalc.py

--- cpe461gpacalc.py
+++ cpe461gpacalc.py
@@ -5,3 +5,13 @@ class App:
 		self.parent = parent
 		self.frame_1 = Frame(parent)
 		self.frame_1.pack()
+        		
+		self.lbl_1 = Label(self.frame_1,text="Current CGPA :")
+		self.lbl_1.grid(row=0,column=0)
+		self.entry_1 = Entry(self.frame_1)
+		self.entry_1.grid(row=0,column=1)
+
+		self.lbl_2 = Label(self.frame_1,text="Units Completed :")
+		self.lbl_2.grid(row=1,column=0)
+		self.entry_2 = Entry(self.frame_1)
+		self.entry_2.grid(row=1,column=1)
