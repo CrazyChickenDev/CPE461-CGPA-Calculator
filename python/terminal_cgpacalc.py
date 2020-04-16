c233a0f39c840a5c0849fa3e5e42ba3604a096e9
CrazyChickenDev <dannychukz15@gmail.com>
2020-04-16 10:50:51 +0100

    added course points and grading system

--- python/terminal_cgpacalc.py
+++ python/terminal_cgpacalc.py
@@ -82,11 +82,11 @@ while n <=6:
     elif scores[n] >= 0 and scores[n] <=39:
         points[n] = 0
     n+=1
-
+    
 units = [5, 4, 4, 1, 1, 1, 0]
 global totalpoints
 totalpoints = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-c = 0
+c=0
 while c<=6:
     totalpoints[c]=units[c]*points[c]
     c+=1
